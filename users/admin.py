a69b11948c78657fddc45922d8e280e37edf86f4
sardor7414 <sardormirzayev74@gmail.com>
2025-08-21 17:47:42 +0500

    admin-py is changed

--- users/admin.py
+++ users/admin.py
@@ -4,9 +4,7 @@ from django.utils.translation import gettext_lazy as _
 from .models import User
 
 
-
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'phone', 'first_name', 'last_name', 'role', 'is_staff', 'is_active')
-
-
+    list_display = ('id', 'phone', 'first_name', 'last_name', 'role', 'reset_code', 'is_reset_verified',
+                    'is_active', 'is_staff', 'is_active')
